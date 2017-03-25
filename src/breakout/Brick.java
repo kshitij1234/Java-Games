8d6c82037be49030fc6e8b75be3a553a7dd6049c
Kshitij Jauhri <kshitijsjauhri@rediffmail.com>
2017-03-25 11:50:55 +0530

    breakout finished

--- src/breakout/Brick.java
+++ src/breakout/Brick.java
@@ -11,7 +11,7 @@ public class Brick extends Sprite{
         this.x = x;
         this.y = y;
 
-        ImageIcon ii = new ImageIcon("src/breakout/images/brick.png");
+        ImageIcon ii = new ImageIcon("src/breakout/images/brickie.png");
         image = ii.getImage();
 
         i_width = image.getWidth(null);
